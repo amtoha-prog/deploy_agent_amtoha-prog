c2107c96e0ff3fe5827e5f355c93372af21f7eb4
amtoha-prog <amtoha@alustudent.com>
2026-06-10 08:29:01 -0700

    Pasting the assests.csv file suing heredoc

--- setup_project.sh
+++ setup_project.sh
@@ -26,3 +26,11 @@ cat > attendance_tracker_$input/Helpers/config.json << 'EOF'
     "total_sessions": 15
 }
 EOF
+
+cat > attendance_tracker_$input/Helpers/assets.csv << 'EOF'
+Email,Names,Attendance Count,Absence Count
+alice@example.com,Alice Johnson,14,1
+bob@example.com,Bob Smith,7,8
+charlie@example.com,Charlie Davis,4,11
+diana@example.com,Diana Prince,15,0
+EOF
