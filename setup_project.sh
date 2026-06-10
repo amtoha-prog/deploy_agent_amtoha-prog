5c5a6edef3e5e710483d2e6e9416b5af5e1bf90e
amtoha-prog <amtoha@alustudent.com>
2026-06-10 05:06:44 -0700

    Checking if the directory exists if not, creates the directory

--- setup_project.sh
+++ setup_project.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
-
+# Asks for the user input
 read -p "Enter your project name: " input
 echo "attendance_tracker_$input"
+
+# This checks for existing directory if not creates the directories
+if [ -d "attendance_tracker_$input" ]
+then
+   echo "Directory already exists"
+   exit 1
+
+else 
+    echo "Creating folders..." 
+    mkdir -p "attendance_tracker_$input/Helpers/" "attendance_tracker_$input/reports"
+fi 
